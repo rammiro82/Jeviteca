a6007781153d5c0a7c582bd5aa25c0526c16a11a
Ramiro García Salazar <rammiro82@gmail.com>
2015-11-15 15:39:33 +0100

    Añadiendo el botón de favorito

--- scripts/directives/ElementoAlbum.js
+++ scripts/directives/ElementoAlbum.js
@@ -1,10 +1,30 @@
 
-angular.module("jeviteca").directive("elementoAlbum", function(){
+angular.module("jeviteca").directive("elementoAlbum",  ["FavoriteService", function(FavoriteService) {
     return {
         restrict: "AE",
         templateUrl: "views/ElementoAlbum.html",
         scope:{
-            album: "="
+            album: "=",
+            id: "=albumId"
+        },
+        link: function(scope)
+        {
+            scope.isLocalStorageEnable = FavoriteService.isLocalStorageEnable;
+            scope.isFavorite = FavoriteService.isFavorite(scope);
+
+            scope.markAs = function(type) {
+                switch(type) {
+                    case true :
+                        FavoriteService.setFav(scope);
+                        break;
+                    case false :
+                        FavoriteService.deleteFav(scope);
+                        break;
+
+                }
+                scope.isFavorite = type;
+            }
+
         }
     };
-});
+}]);
